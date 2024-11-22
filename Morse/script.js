8b17e72185927c8b53e6f3dfdf0a8d7b27c1389d
Paul Vaucher <araghast@gmail.com>
2024-11-22 14:25:06 +0100

    Etape 4 done

--- Morse/script.js
+++ Morse/script.js
@@ -27,34 +27,92 @@ const latinToMorse = {
     'Z': '--..'
 }
 
+const morseToLatin = {
+    '-': "T",
+    '--': "M",
+    '---': "O",
+    '--.': "G",
+    '--.-': "Q",
+    '--..': "Z",
+    '-.': "N",
+    '-.-': "K",
+    '-.--': "Y",
+    '-.-.': "C",
+    '-..': "D",
+    '-..-': "X",
+    '-...': "B",
+    '.': "E",
+    '.-': "A",
+    '.--': "W",
+    '.---': "J",
+    '.--.': "P",
+    '.-.': "R",
+    '.-..': "L",
+    '..': "I",
+    '..-': "U",
+    '..-.': "F",
+    '...': "S",
+    '...-': "V",
+    '....': "H"
+  }
+
+/* Récupère la string et sépare les caractères*/
 function getLatinCharacterList(string) {
     let splittedText = string.split('');
-    console.log(splittedText);
+    //console.log(splittedText);
     return splittedText;
 }
-
 //getLatinCharacterList("Hello, world")
 
+
+/* Transforme une lettre en caractère Morse */
 function translateLatinCharacter(character) {
-    console.log(latinToMorse[character]);
+    //console.log(latinToMorse[character]);
     return latinToMorse[character];
 }
-
 //translateLatinCharacter('A')
 
+/* Transforme un texte en Morse */
 function encode(string) {
     let upperString = string.toUpperCase()
     let textToSplit = getLatinCharacterList(upperString)
     let translatedText = []
     for (let char of textToSplit) {
         translateLatinCharacter(char);
-        if (char === undefined) {
+        /*if (char === undefined) {
             continue
-        };
+        };*/
         translatedText.push(char);
     };
-    console.log(translatedText);
+    //console.log(translatedText);
     return translatedText;
 }
+//encode("Hello, world")
 
-encode("Hello, world")
+/*  */
+function getMorseCharacterList(string) {
+    let splittedMorse = string.split(' ');
+    //console.log(splittedMorse);
+    return splittedMorse;
+}
+
+// getMorseCharacterList(".... . .-.. .-.. --- / .-- --- .-. .-.. -..")
+
+function translateMorseCharacter(character) {
+    console.log(morseToLatin[character]);
+    return morseToLatin[character];
+}
+//translateMorseCharacter("---")
+
+function decode(string) {
+    let morseToSplit = getMorseCharacterList(string)
+    let translatedMorse = []
+    for (let char of morseToSplit) {
+        translateMorseCharacter(char);
+        translatedMorse.push(char);
+    };
+    //console.log(translatedMorse)
+    return translatedMorse
+}
+
+decode(".... . .-.. .-.. --- / .-- --- .-. .-.. -..")
