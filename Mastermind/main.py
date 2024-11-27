4ff7d399762d5847af612059e69fa625dcde08e9
Paul Vaucher <araghast@gmail.com>
2024-11-27 11:08:08 +0100

    pb avec checkValidity, 1 seul passage

--- Mastermind/main.py
+++ Mastermind/main.py
@@ -4,38 +4,34 @@ essais = 12
 
 # Couleurs disponibles
 colorPool = ['bleu', 'rouge', 'jaune', 'vert']
-# print(type(colorPool))
+
 
 # Demande une réponse et insert les couleurs dispos dans le texte
 def answer():
-    global answerInput
     answerInput = input("indiquez votre réponse composée de 2 couleurs parmi cette liste : " + ' '.join(colorPool) +  " (séparées par un espace) : ")
-    #print(answerInput)
     return answerInput
 
-answer()
 
 # Vérifie qu'il y a le bon nombre de réponses et qu'elles sont disponibles
 def checkValidity():
+    answerInput = answer()
     answerList = answerInput.split(' ')
-    print(answerList)
     delimiter = ", "
     print("Couleurs choisies = " + delimiter.join(answerList))
 
- 
-    for color in answerList:
-        if color not in colorPool or len(answerList) != 2:
+    while True:
+        for color in answerList:
+            if color not in colorPool:
+                print ("Votre réponse ne fait pas partie de la liste. Entrez une nouvelle réponse")
+                continue
+            elif len(answerList) != 2:
+                print ("Votre réponse n'as pas le bon nombre d'éléments. Entrez une nouvelle réponse")
+                continue
+        else:
+            return answerList      
         
-            print ("Votre réponse ne fait pas partie de la liste ou n'as pas le bon nombre d'éléments. Entrez une nouvelle réponse")
-            answer()
-        
-    else:
-        # print("good")
-        return answerList       
-        
-checkValidity()
 
-# 
+# Retourne True si bonne solution, sinon donne le nb de réponses (à valider)
 def gameCore():
     answerList = checkValidity()
     goodColor = 0
@@ -46,15 +42,30 @@ def gameCore():
     if solution == answerList:
         return True
     else:
-        checkColor = len(set(solution)&set(answerList))
-        goodColor += checkColor
+        goodColor = len(set(solution)&set(answerList))
+        print(goodColor)
         checkPosition = []
         for i in range(len(solution)):
             checkPosition.append(solution[i] == answerList[i])
         goodPosition += sum(checkPosition)
-        print(str(answerList) + "-> " + str(goodPosition) + " de la bonne couleur à la bonne position / " + str(goodColor) + " de la bonne couleur à la bonne position")
+        print(goodPosition)
+        print(str(answerList) + "-> " + str(goodPosition) + " de la bonne couleur à la bonne position / " + str(goodColor) + " de la bonne couleur à la mauvaise position")
         return False
         
     
-gameCore()
-        
+# Affiche "gagné" si gameCore() renvoie True, lance un nouvel essai si Fale et affiche "perdu" si essais=0 
+def gameCycle ():
+    state = gameCore()
+    global essais
+    while essais > 0:
+        if state == False:
+            essais -= 1
+            print ("il vous reste " + str(essais) +" essais")
+            answer()
+        else: 
+            print ("Bravo ! Vous avez gagné !")
+            break
+    if essais == 0:
+        print ("Vous avez perdu")
+
+gameCycle()
